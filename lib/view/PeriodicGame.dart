783f0d3272e6c898030ac6c2e1e69d8fcf7476a5
Ethan Lundgren <lundg273@d.umn.edu>
2026-02-24 17:48:17 -0600

    Continuing to add to Periodic Game

--- lib/view/PeriodicGame.dart
+++ lib/view/PeriodicGame.dart
@@ -1,4 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:periodic_table/Elements.dart';
+import 'package:periodic_table/Elements/Actinium.dart';
+import 'package:periodic_table/Elements/Hydrogen.dart';
+import 'package:periodic_table/Elements/Helium.dart';
+import 'package:periodic_table/Elements/Lithium.dart';
+import 'package:periodic_table/Elements/Beryllium.dart';
+import 'package:periodic_table/Elements/Boron.dart';
+import 'package:periodic_table/Elements/Carbon.dart';
+import 'package:periodic_table/Elements/Nitrogen.dart';
+import 'package:periodic_table/Elements/Oxygen.dart';
+import 'package:periodic_table/Elements/Fluorine.dart';
+import 'package:periodic_table/Elements/Neon.dart';
+import 'package:periodic_table/Elements/Sodium.dart';
+import 'package:periodic_table/Elements/Magnesium.dart';
+import 'package:periodic_table/Elements/Aluminum.dart';
+import 'package:periodic_table/Elements/Silicon.dart';
+import 'package:periodic_table/Elements/Phosphorus.dart';
+import 'package:periodic_table/Elements/Sulfur.dart';
+import 'package:periodic_table/Elements/Chlorine.dart';
+import 'package:periodic_table/Elements/Argon.dart';
+import 'package:periodic_table/Elements/Potassium.dart';
+import 'package:periodic_table/Elements/Calcium.dart';
+import 'package:periodic_table/Elements/Scandium.dart';
+import 'package:periodic_table/Elements/Titanium.dart';
+import 'package:periodic_table/Elements/Vanadium.dart';
+import 'package:periodic_table/Elements/Chromium.dart';
+import 'package:periodic_table/Elements/Manganese.dart';
+import 'package:periodic_table/Elements/Iron.dart';
+import 'package:periodic_table/Elements/Cobalt.dart';
+import 'package:periodic_table/Elements/Nickel.dart';
+import 'package:periodic_table/Elements/Copper.dart';
+import 'package:periodic_table/Elements/Zinc.dart';
+import 'package:periodic_table/Elements/Gallium.dart';
+import 'package:periodic_table/Elements/Germanium.dart';
+import 'package:periodic_table/Elements/Arsenic.dart';
+import 'package:periodic_table/Elements/Selenium.dart';
+import 'package:periodic_table/Elements/Bromine.dart';
+import 'package:periodic_table/Elements/Krypton.dart';
+import 'package:periodic_table/Elements/Rubidium.dart';
+import 'package:periodic_table/Elements/Strontium.dart';
+import 'package:periodic_table/Elements/Yttrium.dart';
+import 'package:periodic_table/Elements/Zirconium.dart';
+import 'package:periodic_table/Elements/Niobium.dart';
+import 'package:periodic_table/Elements/Molybdenum.dart';
+import 'package:periodic_table/Elements/Technetium.dart';
+import 'package:periodic_table/Elements/Ruthenium.dart';
+import 'package:periodic_table/Elements/Rhodium.dart';
+import 'package:periodic_table/Elements/Palladium.dart';
+import 'package:periodic_table/Elements/Silver.dart';
+import 'package:periodic_table/Elements/Cadmium.dart';
+import 'package:periodic_table/Elements/Indium.dart';
+import 'package:periodic_table/Elements/Tin.dart';
+import 'package:periodic_table/Elements/Antimony.dart';
+import 'package:periodic_table/Elements/Tellurium.dart';
+import 'package:periodic_table/Elements/Iodine.dart';
+import 'package:periodic_table/Elements/Xenon.dart';
+import 'package:periodic_table/Elements/Cesium.dart';
+import 'package:periodic_table/Elements/Barium.dart';
+import 'package:periodic_table/Elements/Lanthanum.dart';
+import 'package:periodic_table/Elements/Cerium.dart';
+import 'package:periodic_table/Elements/Praseodymium.dart';
+import 'package:periodic_table/Elements/Neodymium.dart';
+import 'package:periodic_table/Elements/Promethium.dart';
+import 'package:periodic_table/Elements/Samarium.dart';
+import 'package:periodic_table/Elements/Europium.dart';
+import 'package:periodic_table/Elements/Gadolinium.dart';
+import 'package:periodic_table/Elements/Terbium.dart';
+import 'package:periodic_table/Elements/Dysprosium.dart';
+import 'package:periodic_table/Elements/Holmium.dart';
+import 'package:periodic_table/Elements/Erbium.dart';
+import 'package:periodic_table/Elements/Thulium.dart';
+import 'package:periodic_table/Elements/Ytterbium.dart';
+import 'package:periodic_table/Elements/Lutetium.dart';
+import 'package:periodic_table/Elements/Hafnium.dart';
+import 'package:periodic_table/Elements/Tantalum.dart';
+import 'package:periodic_table/Elements/Tungsten.dart';
+import 'package:periodic_table/Elements/Rhenium.dart';
+import 'package:periodic_table/Elements/Osmium.dart';
+import 'package:periodic_table/Elements/Iridium.dart';
+import 'package:periodic_table/Elements/Platinum.dart';
+import 'package:periodic_table/Elements/Gold.dart';
+import 'package:periodic_table/Elements/Mercury.dart';
+import 'package:periodic_table/Elements/Thallium.dart';
+import 'package:periodic_table/Elements/Lead.dart';
+import 'package:periodic_table/Elements/Bismuth.dart';
+import 'package:periodic_table/Elements/Polonium.dart';
+import 'package:periodic_table/Elements/Astatine.dart';
+import 'package:periodic_table/Elements/Radon.dart';
+import 'package:periodic_table/Elements/Francium.dart';
+import 'package:periodic_table/Elements/Radium.dart';
+import 'package:periodic_table/Elements/Thorium.dart';
+import 'package:periodic_table/Elements/Protactinium.dart';
+import 'package:periodic_table/Elements/Uranium.dart';
+import 'package:periodic_table/Elements/Neptunium.dart';
+import 'package:periodic_table/Elements/Plutonium.dart';
+import 'package:periodic_table/Elements/Americium.dart';
+import 'package:periodic_table/Elements/Curium.dart';
+import 'package:periodic_table/Elements/Berkelium.dart';
+import 'package:periodic_table/Elements/Californium.dart';
+import 'package:periodic_table/Elements/Einsteinium.dart';
+import 'package:periodic_table/Elements/Fermium.dart';
+import 'package:periodic_table/Elements/Mendelevium.dart';
+import 'package:periodic_table/Elements/Nobelium.dart';
+import 'package:periodic_table/Elements/Lawrencium.dart';
+import 'package:periodic_table/Elements/Rutherfordium.dart';
+import 'package:periodic_table/Elements/Dubnium.dart';
+import 'package:periodic_table/Elements/Seaborgium.dart';
+import 'package:periodic_table/Elements/Bohrium.dart';
+import 'package:periodic_table/Elements/Hassium.dart';
+import 'package:periodic_table/Elements/Meitnerium.dart';
+import 'package:periodic_table/Elements/Darmstadtium.dart';
+import 'package:periodic_table/Elements/Roentgenium.dart';
+import 'package:periodic_table/Elements/Copernicium.dart';
+import 'package:periodic_table/Elements/Nihonium.dart';
+import 'package:periodic_table/Elements/Flerovium.dart';
+import 'package:periodic_table/Elements/Moscovium.dart';
+import 'package:periodic_table/Elements/Livermorium.dart';
+import 'package:periodic_table/Elements/Tennessine.dart';
+import 'package:periodic_table/Elements/Oganesson.dart';
+import 'package:periodic_table/presenter/periodic_presenter.dart';
 
 class PeriodicGame extends StatefulWidget{
   const PeriodicGame({super.key});
@@ -6,6 +126,10 @@ class PeriodicGame extends StatefulWidget{
   @override
   State<PeriodicGame> createState() => _PeriodicGameScreenState();
 }
+final PeriodicPresenter _presenter = PeriodicPresenter();
+
+String? _selectedElement;
+
 int ones = 0;
 int twos = 0;
 int twop = 0;
@@ -89,17 +213,56 @@ class _PeriodicGameScreenState extends State<PeriodicGame> {
   });
   }
 
+  void check(){
+
+  }
+
   @override
   Widget build(BuildContext context){
+    final List<Elements> elementlist = _presenter.getlist();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Perodic Table'),
         actions: [
+          SizedBox(
+          height: 30,
+          width: 75,
+          child : DropdownButton<String>(
+            hint: const Text(
+              'Select Element',
+              style: TextStyle(color: Colors.white),
+            ),
+            dropdownColor: Colors.blue[100],
+            isDense: true,
+            isExpanded: true,
+            value: _selectedElement,
+            onChanged: (value){
+              setState(() {
+                _selectedElement = value;
+              });
+            },
+            items: [
+              const DropdownMenuItem<String>(
+                value: null,
+                child: Text('No Element Selected'),
+              ),
+              ...elementlist.map(
+                (name) => DropdownMenuItem(value: name.getElementname(), child: Text(name.getElementname())),
+              ),
+            ],
+          ),),
           IconButton(
           icon: const Icon(
             Icons.delete
           ),
           onPressed: clear,
+          ),
+          IconButton(
+          icon: const Icon(
+            Icons.check
+          ),
+          onPressed: check,
           ),
         ],),
       body: ListView(
@@ -137,6 +300,295 @@ class _PeriodicGameScreenState extends State<PeriodicGame> {
             ),
           ),
           child: const Text('2s'),
+          ),
+          ElevatedButton(
+          onPressed: () {update2p(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('2p'),
+          ),
+          ElevatedButton(
+          onPressed: () {update3s(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('3s'),
+          ),
+          ElevatedButton(
+          onPressed: () {update3p(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('3p'),
+          ),
+          ElevatedButton(
+          onPressed: () {update3d(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('3d'),
+          ),
+          ElevatedButton(
+          onPressed: () {update4s(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('4s'),
+          ),
+          ElevatedButton(
+          onPressed: () {update4p(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('4p'),
+          ),
+          ElevatedButton(
+          onPressed: () {update4d(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('4d'),
+          ),
+          ElevatedButton(
+          onPressed: () {update4f(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('4f'),
+          ),
+          ElevatedButton(
+          onPressed: () {update5s(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('5s'),
+          ),
+          ElevatedButton(
+          onPressed: () {update5p(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('5p'),
+          ),
+          ElevatedButton(
+          onPressed: () {update5d(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('5d'),
+          ),
+          ElevatedButton(
+          onPressed: () {update5f(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('5f'),
+          ),
+          ElevatedButton(
+          onPressed: () {update6s(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('6s'),
+          ),
+          ElevatedButton(
+          onPressed: () {update6p(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('6p'),
+          ),
+          ElevatedButton(
+          onPressed: () {update6d(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('6d'),
+          ),
+          ElevatedButton(
+          onPressed: () {update7s(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('7s'),
+          ),
+          ElevatedButton(
+          onPressed: () {update7p(1);},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.black,
+            elevation: 10,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text('7p'),
           ),
         ]
     )

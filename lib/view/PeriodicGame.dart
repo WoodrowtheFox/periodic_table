21e2ccbc27ac1b38898f23dda85431f6fa7c9287
Ethan Lundgren <lundg273@d.umn.edu>
2026-02-24 16:11:07 -0600

    Added checking for the quiz

---
+++ lib/view/PeriodicGame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:periodic_table/model/Feedback_model.dart';
+import 'package:periodic_table/model/Feedback_model.dart' as feedbackclass;
+import '../presenter/feedback_presenter.dart';
+import '../widgets/add_fab.dart';
+
+class PeriodicGame extends StatefulWidget{
+  const PeriodicGame({super.key});
+
+  @override
+  State<PeriodicGame> createState() => _PeriodicGameScreenState();
+}
+class _PeriodicGameScreenState extends State<PeriodicGame> {
+  
+}

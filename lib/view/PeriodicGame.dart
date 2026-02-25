700f86ad96e487bf2eff53781c8d43bd78e43cb0
lundg273 <lundg273@d.umn.edu>
2026-02-24 21:39:04 -0600

    Just need to tweak the elements

--- lib/view/PeriodicGame.dart
+++ lib/view/PeriodicGame.dart
@@ -1,123 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:periodic_table/Elements.dart';
-import 'package:periodic_table/Elements/Actinium.dart';
-import 'package:periodic_table/Elements/Hydrogen.dart';
-import 'package:periodic_table/Elements/Helium.dart';
-import 'package:periodic_table/Elements/Lithium.dart';
-import 'package:periodic_table/Elements/Beryllium.dart';
-import 'package:periodic_table/Elements/Boron.dart';
-import 'package:periodic_table/Elements/Carbon.dart';
-import 'package:periodic_table/Elements/Nitrogen.dart';
-import 'package:periodic_table/Elements/Oxygen.dart';
-import 'package:periodic_table/Elements/Fluorine.dart';
-import 'package:periodic_table/Elements/Neon.dart';
-import 'package:periodic_table/Elements/Sodium.dart';
-import 'package:periodic_table/Elements/Magnesium.dart';
-import 'package:periodic_table/Elements/Aluminum.dart';
-import 'package:periodic_table/Elements/Silicon.dart';
-import 'package:periodic_table/Elements/Phosphorus.dart';
-import 'package:periodic_table/Elements/Sulfur.dart';
-import 'package:periodic_table/Elements/Chlorine.dart';
-import 'package:periodic_table/Elements/Argon.dart';
-import 'package:periodic_table/Elements/Potassium.dart';
-import 'package:periodic_table/Elements/Calcium.dart';
-import 'package:periodic_table/Elements/Scandium.dart';
-import 'package:periodic_table/Elements/Titanium.dart';
-import 'package:periodic_table/Elements/Vanadium.dart';
-import 'package:periodic_table/Elements/Chromium.dart';
-import 'package:periodic_table/Elements/Manganese.dart';
-import 'package:periodic_table/Elements/Iron.dart';
-import 'package:periodic_table/Elements/Cobalt.dart';
-import 'package:periodic_table/Elements/Nickel.dart';
-import 'package:periodic_table/Elements/Copper.dart';
-import 'package:periodic_table/Elements/Zinc.dart';
-import 'package:periodic_table/Elements/Gallium.dart';
-import 'package:periodic_table/Elements/Germanium.dart';
-import 'package:periodic_table/Elements/Arsenic.dart';
-import 'package:periodic_table/Elements/Selenium.dart';
-import 'package:periodic_table/Elements/Bromine.dart';
-import 'package:periodic_table/Elements/Krypton.dart';
-import 'package:periodic_table/Elements/Rubidium.dart';
-import 'package:periodic_table/Elements/Strontium.dart';
-import 'package:periodic_table/Elements/Yttrium.dart';
-import 'package:periodic_table/Elements/Zirconium.dart';
-import 'package:periodic_table/Elements/Niobium.dart';
-import 'package:periodic_table/Elements/Molybdenum.dart';
-import 'package:periodic_table/Elements/Technetium.dart';
-import 'package:periodic_table/Elements/Ruthenium.dart';
-import 'package:periodic_table/Elements/Rhodium.dart';
-import 'package:periodic_table/Elements/Palladium.dart';
-import 'package:periodic_table/Elements/Silver.dart';
-import 'package:periodic_table/Elements/Cadmium.dart';
-import 'package:periodic_table/Elements/Indium.dart';
-import 'package:periodic_table/Elements/Tin.dart';
-import 'package:periodic_table/Elements/Antimony.dart';
-import 'package:periodic_table/Elements/Tellurium.dart';
-import 'package:periodic_table/Elements/Iodine.dart';
-import 'package:periodic_table/Elements/Xenon.dart';
-import 'package:periodic_table/Elements/Cesium.dart';
-import 'package:periodic_table/Elements/Barium.dart';
-import 'package:periodic_table/Elements/Lanthanum.dart';
-import 'package:periodic_table/Elements/Cerium.dart';
-import 'package:periodic_table/Elements/Praseodymium.dart';
-import 'package:periodic_table/Elements/Neodymium.dart';
-import 'package:periodic_table/Elements/Promethium.dart';
-import 'package:periodic_table/Elements/Samarium.dart';
-import 'package:periodic_table/Elements/Europium.dart';
-import 'package:periodic_table/Elements/Gadolinium.dart';
-import 'package:periodic_table/Elements/Terbium.dart';
-import 'package:periodic_table/Elements/Dysprosium.dart';
-import 'package:periodic_table/Elements/Holmium.dart';
-import 'package:periodic_table/Elements/Erbium.dart';
-import 'package:periodic_table/Elements/Thulium.dart';
-import 'package:periodic_table/Elements/Ytterbium.dart';
-import 'package:periodic_table/Elements/Lutetium.dart';
-import 'package:periodic_table/Elements/Hafnium.dart';
-import 'package:periodic_table/Elements/Tantalum.dart';
-import 'package:periodic_table/Elements/Tungsten.dart';
-import 'package:periodic_table/Elements/Rhenium.dart';
-import 'package:periodic_table/Elements/Osmium.dart';
-import 'package:periodic_table/Elements/Iridium.dart';
-import 'package:periodic_table/Elements/Platinum.dart';
-import 'package:periodic_table/Elements/Gold.dart';
-import 'package:periodic_table/Elements/Mercury.dart';
-import 'package:periodic_table/Elements/Thallium.dart';
-import 'package:periodic_table/Elements/Lead.dart';
-import 'package:periodic_table/Elements/Bismuth.dart';
-import 'package:periodic_table/Elements/Polonium.dart';
-import 'package:periodic_table/Elements/Astatine.dart';
-import 'package:periodic_table/Elements/Radon.dart';
-import 'package:periodic_table/Elements/Francium.dart';
-import 'package:periodic_table/Elements/Radium.dart';
-import 'package:periodic_table/Elements/Thorium.dart';
-import 'package:periodic_table/Elements/Protactinium.dart';
-import 'package:periodic_table/Elements/Uranium.dart';
-import 'package:periodic_table/Elements/Neptunium.dart';
-import 'package:periodic_table/Elements/Plutonium.dart';
-import 'package:periodic_table/Elements/Americium.dart';
-import 'package:periodic_table/Elements/Curium.dart';
-import 'package:periodic_table/Elements/Berkelium.dart';
-import 'package:periodic_table/Elements/Californium.dart';
-import 'package:periodic_table/Elements/Einsteinium.dart';
-import 'package:periodic_table/Elements/Fermium.dart';
-import 'package:periodic_table/Elements/Mendelevium.dart';
-import 'package:periodic_table/Elements/Nobelium.dart';
-import 'package:periodic_table/Elements/Lawrencium.dart';
-import 'package:periodic_table/Elements/Rutherfordium.dart';
-import 'package:periodic_table/Elements/Dubnium.dart';
-import 'package:periodic_table/Elements/Seaborgium.dart';
-import 'package:periodic_table/Elements/Bohrium.dart';
-import 'package:periodic_table/Elements/Hassium.dart';
-import 'package:periodic_table/Elements/Meitnerium.dart';
-import 'package:periodic_table/Elements/Darmstadtium.dart';
-import 'package:periodic_table/Elements/Roentgenium.dart';
-import 'package:periodic_table/Elements/Copernicium.dart';
-import 'package:periodic_table/Elements/Nihonium.dart';
-import 'package:periodic_table/Elements/Flerovium.dart';
-import 'package:periodic_table/Elements/Moscovium.dart';
-import 'package:periodic_table/Elements/Livermorium.dart';
-import 'package:periodic_table/Elements/Tennessine.dart';
-import 'package:periodic_table/Elements/Oganesson.dart';
 import 'package:periodic_table/presenter/periodic_presenter.dart';
 
 class PeriodicGame extends StatefulWidget{
@@ -129,6 +11,8 @@ class PeriodicGame extends StatefulWidget{
 final PeriodicPresenter _presenter = PeriodicPresenter();
 
 String? _selectedElement;
+final List<Elements> elementlist = _presenter.getlist();
+String checkbool = "false";
 
 int ones = 0;
 int twos = 0;
@@ -210,20 +94,31 @@ class _PeriodicGameScreenState extends State<PeriodicGame> {
      sixd = 0;
      sevens = 0;
      sevenp = 0;
+     checkbool = "false";
   });
   }
 
   void check(){
-
+    for(Elements i in elementlist){
+      if(i.getElementname() == _selectedElement){
+        Map<String, int> myhash = i.getshelltotals();
+        if(myhash["1s"] == ones && myhash["2s"] == twos && myhash["2p"] == twop && myhash["3s"] == threes && myhash["3p"] == threep &&
+          myhash["3d"] == threed && myhash["4s"] == fours && myhash["4p"] == fourp && myhash["4d"] == fourd && myhash["4f"] == fourf &&
+          myhash["5s"] == fives && myhash["5p"] == fivep && myhash["5d"] == fived && myhash["5f"] == fivef && myhash["6s"] == sixs &&
+          myhash["6p"] == sixp && myhash["6d"] == sixd && myhash["7s"] == sevens && myhash["7p"] == sevenp){
+          setState(() {
+            checkbool = "true";
+          });
+        }
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context){
-    final List<Elements> elementlist = _presenter.getlist();
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Perodic Table'),
+        title: Text('Perodic Table -> Is your answer correct? - ' + checkbool),
         actions: [
           SizedBox(
           height: 30,
